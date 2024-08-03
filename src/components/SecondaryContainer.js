42e75f7028b3d0b5a334dd5b1beec70b09801f8b
Dawsons Geek <157927926+dawsonsgeek@users.noreply.github.com>
2024-08-02 22:34:25 -0400

    added movie posters in containers but image source is broken

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,8 +1,26 @@
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
+
+
 const SecondaryContainer = () => {
+    const movies = useSelector((store) => store.movies); 
+
     return (
-        <div>
-            <h1>Secondary Container</h1>
+        movies.nowPlayingMovies && (
+        <div className="bg-black">
+            <div className="-mt-52 pl-12 relative z-20">
+                <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+                <MovieList title={"Popular"} movies={movies.popularMovies} />
+                <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
+                <MovieList title={"Upcoming"} movies={movies.upcomingMovies} />
+            </div>
         </div>
-    )
+        )
+
+    // <div>
+        // {/* <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} /> */}
+
+        // </div>
+    );
 };
 export default SecondaryContainer; 
