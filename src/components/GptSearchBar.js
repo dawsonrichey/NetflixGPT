9016768fdb67327fa7b2ccdd68def7b97ef1b5cd
Dawsons Geek <157927926+dawsonsgeek@users.noreply.github.com>
2024-08-04 14:23:50 -0400

    improved syle on gptsearch page

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const GptSearchBar = () => {
   return (
-    <div className='pt-[35%]'>
+    <div className='pt-[10%] flex justify-center'>
         <form className="w-1/2 grid grid-cols-12 bg-black opacity-90">
             <input 
             type='text'
-            className="w-full md:w-1/2 p-4 text-lg rounded-lg"
+            className="p-4 m-4  col-span-9"
             placeholder='What would you like to watch today?' />
-            <button className="p-4 ml-4 bg-red-700 text-white rounded-lg">Search</button>
+            <button className="col-span-3 py-2 m-4px-4  bg-red-700 text-white rounded-lg">Search</button>
             </form>
     </div>
   )
