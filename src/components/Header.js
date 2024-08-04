e3f68eb7ef795069739cbf7f7232bb254d9156c7
Dawsons Geek <157927926+dawsonsgeek@users.noreply.github.com>
2024-08-04 15:22:35 -0400

    added language configuration setting

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,17 @@ import { onAuthStateChanged, signOut } from 'firebase/auth';
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { LOGO } from '../utils/constants';
+import { LOGO, SUPPORTED_LANGUAGES } from '../utils/constants';
 import { auth } from '../utils/firebase';
 import { addUser, removeUser } from '../utils/userSlice';
 import { toggleGptSearchView } from '../utils/gptSlice';
+import { changeLanguage } from '../utils/configSlice';
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
   const handleSignOut = () => {
     signOut(auth)
     .then(() => {
@@ -46,6 +48,10 @@ const Header = () => {
     // navigate("/gptsearch");
     dispatch(toggleGptSearchView());
   };
+  const handleLanguageChange = (e) => {
+    // console.log(event.target.value);
+    dispatch(changeLanguage(e.target.value));
+  };
 
   return (
     <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
@@ -57,7 +63,17 @@ const Header = () => {
    {
    user &&(
     <div className='flex p-2'>
-      <button className='bg-purple-600 px-4 py-1 mx-2 rounded-lg' onClick={handleGptSearchClick}>GPT Search</button>
+      {showGptSearch && (
+        <select  className='bg-gray-900 text-white p-1 rounded-lg' onChange={handleLanguageChange}>
+          {SUPPORTED_LANGUAGES.map((lang) => (
+            <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>
+          ))} 
+        </select>
+      )}
+
+      <button className='bg-purple-600 px-4 py-1 mx-2 rounded-lg' onClick={handleGptSearchClick}>
+        {showGptSearch ? "Homepage" : "GPT Search"}
+      </button>
           <img alt="usericon" className='w-12 h-12' src={user?.photoURL} />
           <button onClick={handleSignOut} className='font-bold text-white'>(Sign Out)</button>
         </div>
