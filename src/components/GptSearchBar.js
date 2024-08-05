5ebd8dbb51db27550d9de250825ce8ca4aaf3ca3
Dawsons Geek <157927926+dawsonsgeek@users.noreply.github.com>
2024-08-05 16:46:11 -0400

    returning ai search results

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,26 +1,55 @@
 import React, { useRef } from 'react'
 import lang from '../utils/languageConstants';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import openai from '../utils/openai';
+import { API_OPTIONS } from '../utils/constants';
+import { addGptMovieResult } from '../utils/gptSlice';
+// import useMovieTrailer from '../hooks/useMovieTrailer';
+// import { json } from 'body-parser';
 
 
 const GptSearchBar = () => {
+  const dispatch = useDispatch();
   const langKey = useSelector((store) => store.config.lang);
   const searchText = useRef(null);
+
+  // search movie TBDB
+  const searchMovieTMDB = async (movie) => {
+    const data = await fetch(
+    "https://api.themoviedb.org/3/search/movie?query=" + movie + "&include_adult=false&language=en-US&page=1", API_OPTIONS
+    
+  );
+  const json = await data.json();
+
+  return json.results;
+  };
 
   const handleGptSearchClick = async() => {
     console.log(searchText.current.value);
     // Make an API call to GPT API 
 
-    const gptQuery = "Act as a movie recommendation system and suggest some movies for the query : " + searchText.current.value + ". Only give me names of 5 movies, comma separated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya";
+    const gptQuery = "Act as a movie recommendation system and suggest some movies for the query : " + searchText.current.value + ". Only give me names of 2 movies, comma separated like the example result given ahead. Example Result: Gadar, Sholay";
 
     const gptResults = await openai.chat.completions.create({
           messages: [{ role: 'user', content: gptQuery }],
-          model: 'gpt-3.5-turbo',
+          model: 'gpt-4o-mini-2024-07-18',
         });
+        if(!gptResults.choices){ // TO DO: Handle the error
+        }
+        console.log(gptResults.choices?.[0]?.message?.content);
 
-        console.log(gptResults.choices);
+        const gptMovies = gptResults.choices?.[0]?.message?.content.split(",");
+
+        // for each movie i will search TMDB API
+        const promiseArray = gptMovies.map((movie) => searchMovieTMDB(movie));        
+
+        const tmdbResults = await Promise.all(promiseArray);
+
+        console.log(tmdbResults);
+
+        dispatch(addGptMovieResult({movieNames: gptMovies, addGptMovieResult: tmdbResults}));
   };
+
   return (
     <div className='pt-[10%] flex justify-center'>
         <form className="w-1/2 grid grid-cols-12 bg-black opacity-90" onSubmit={(e) => e.preventDefault()}>
